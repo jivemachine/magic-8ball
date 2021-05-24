aa6adeed37e5617da017ff8eb9de540a7398c759
jivemachine <the.jeremy.cobb@gmail.com>
2021-05-24 16:35:56 -0500

    changed switch statement to for/else if loop

--- main.js
+++ main.js
@@ -1,25 +1,36 @@
-userName = '';
+userName = 'Jeremy';
 userName ? console.log('Hello, ' + userName + '!') : console.log('Hello!');
 const userQuestion = 'Will I become a billionaire?';
-console.log(userName + 'asked: ' + userQuestion);
+console.log(userName + ' asked: ' + userQuestion);
 const randomNumber = Math.floor(Math.random() * 8);
 let eightBall = '';
-switch (randomNumber) {
-  case 0:
+// switch (randomNumber) {
+//   case 0:
+//   eightBall = 'It is certain';
+//   case 1:
+//   eightBall = 'It id decidedly so';
+//   case 2:
+//   eightBall = 'Reply hazy, try again.';
+//   case 3:
+//   eightBall = 'Cannot predict right now';
+//   case 4:
+//   eightBall = 'Don\'t Count on it';
+//   case 5:
+//   eightBall = 'My sources say thats gunna be a no, bud.';
+//   case 6:
+//   eightBall = 'Outlook, not good';
+//   case 7:
+//   eightBall = 'Yes.';
+// }
+
+if (randomNumber == 0) {
   eightBall = 'It is certain';
-  case 1:
-  eightBall = 'It id decidedly so';
-  case 2:
-  eightBall = 'Reply hazy, try again.';
-  case 3:
-  eightBall = 'Cannot predict right now';
-  case 4:
-  eightBall = 'Don\'t Count on it';
-  case 5:
-  eightBall = 'My sources say thats gunna be a no, bud.';
-  case 6:
-  eightBall = 'Outlook, not good';
-  case 7:
-  eightBall = 'Yes.';
+} else if (randomNumber == 1) {
+  eightBall = 'It is decidely so'; 
+} else if (randomNumber == 2) {
+  eightBall = "Reply hazy, try again";
+} else if (randomNumber == 3) {
+  eightBall = 'Yes';
 }
+
 console.log(`The eight ball has answered: ${eightBall}`);
